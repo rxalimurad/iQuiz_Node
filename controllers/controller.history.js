e1ccf2dc4d63eab435dc4494828968e27345bd6e
alimurad <amurad@i2cinc.com>
2023-11-08 18:03:28 +0500

    -- add history with new approch

--- controllers/controller.history.js
+++ controllers/controller.history.js
@@ -4,70 +4,71 @@ const User = require("../models/model.user");
 const Quiz = require("../models/model.quiz");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("../middlewares/middleware.async");
-const ErrorResponse = require('../utils/errorResponse'); 
+const ErrorResponse = require("../utils/errorResponse");
 
-exports.addHistory = asyncHandler(async(req, res, next) => {
+exports.addHistory = asyncHandler(async (req, res, next) => {
   if (req.headers.authorization === undefined) {
-    return next(new ErrorResponse(`Unauthorized`, 401))
-}
-    let quizID = req.body.quizID;
-    let token = req.headers.authorization.split(" ")[1];
-    let decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ phone: decoded.phone });
-    const category = await Quiz.findById(quizID).populate("questions");
-    let anwsers = req.body.anwsers;
-    if (anwsers.length === category.questions.length) {
+    return next(new ErrorResponse(`Unauthorized`, 401));
+  }
+  let quizID = req.body.quiz;
+  let token = req.headers.authorization.split(" ")[1];
+  let decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await User.findOne({ phone: decoded.phone });
+
+  const category = await Quiz.findById(quizID).populate("questions");
+   let anwsers = req.body.anwsers;
+  if (anwsers.length === category.questions.length) {
       let history = await History.create({
-        user: user._id,
         quiz: quizID,
-        anwsers: req.body.anwsers,
-      });
-      res.status(201).json({
-        success: true,
-        data: history,
-      });
-    } else {
-      return next(new ErrorResponse(`Input anwsers not match with questions`), 401)
-    }
-  
+        anwsers: anwsers,
+        user: user._id,
+      })
+      res.send(history)
+        
+  } else {
+    return next(
+      new ErrorResponse(`Input anwsers not match with questions`),
+      401
+    );
+  }
 });
 
-exports.fetchAllHistory = asyncHandler(async(req, res, next) => {
+exports.fetchAllHistory = asyncHandler(async (req, res, next) => {
   if (req.headers.authorization === undefined) {
-    return next(new ErrorResponse(`Unauthorized`, 401))
-}
-    let token = req.headers.authorization.split(" ")[1];
-    let decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ phone: decoded.phone }).populate({
-      path: "histories",
+    return next(new ErrorResponse(`Unauthorized`, 401));
+  }
+  let token = req.headers.authorization.split(" ")[1];
+  let decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const user = await User.findOne({ phone: decoded.phone }).populate({
+    path: "histories",
+    populate: {
+      path: "quiz",
+      model: "Quiz",
       populate: {
-        path: "quiz",
-        model: "Quiz",
-        populate: {
-          path: "questions",
-          model: "Question",
-        },
+        path: "questions",
+        model: "Question",
       },
-    });
-    let histories = user.histories;
-    let selectedAnwers = histories.map((history) => history.anwsers);
-    let anwsers = histories.map((history) =>
-      history.quiz.questions.map((quiz) => quiz.correctAnswer)
-    );
-    let name = histories.map((history) => history.quiz.name);
-    let timestamp = histories.map((history) => history.timestamp);
+    },
+  });
+  let histories = user.histories;
+  let selectedAnwers = histories.map((history) => history.anwsers);
+  let anwsers = histories.map((history) =>
+    history.quiz.questions.map((quiz) => quiz.correctAnswer)
+  );
+  let name = histories.map((history) => history.quiz.name);
+  let timestamp = histories.map((history) => history.timestamp);
 
-    let zippedArray = selectedAnwers.map((selectedAnswer, index) => {
-      return {
-        id: histories[index]._id,
-        name: name[index],
-        correct: countMatchingElements(selectedAnwers[index], anwsers[index]),
-        totalQuestions: selectedAnswer.length,
-        timestamp: timestamp[index],
-      };
-    });
-    res.status(200).json({ results: zippedArray });
- 
+  let zippedArray = selectedAnwers.map((selectedAnswer, index) => {
+    return {
+      id: histories[index]._id,
+      name: name[index],
+      correct: countMatchingElements(selectedAnwers[index], anwsers[index]),
+      totalQuestions: selectedAnswer.length,
+      timestamp: timestamp[index],
+    };
+  });
+  res.status(200).json({ results: zippedArray });
+
   function countMatchingElements(arr1, arr2) {
     let count = 0;
     for (let i = 0; i < arr1.length; i++) {
@@ -81,8 +82,8 @@ exports.fetchAllHistory = asyncHandler(async(req, res, next) => {
 
 exports.fetchDetailHistory = asyncHandler(async (req, res, next) => {
   if (req.headers.authorization === undefined) {
-    return next(new ErrorResponse(`Unauthorized`, 401))
-}
+    return next(new ErrorResponse(`Unauthorized`, 401));
+  }
   let token = req.headers.authorization.split(" ")[1];
   let decoded = jwt.verify(token, process.env.JWT_SECRET);
   let quizID = req.params.id;
@@ -95,12 +96,19 @@ exports.fetchDetailHistory = asyncHandler(async (req, res, next) => {
       },
     });
   let zippedArray = history.quiz.questions.map((question, index) => {
-      return {
-          question: question.question,
-          isCorrect: history.quiz.questions[index].correctAnswer === history.anwsers[index],
-          correct: history.quiz.questions[index].options[getCorrectAnswer(history.quiz.questions[index].correctAnswer) - 1],
-          selected: history.quiz.questions[index].options[getCorrectAnswer(history.anwsers[index]) - 1],
-      }
+    return {
+      question: question.question,
+      isCorrect:
+        history.quiz.questions[index].correctAnswer === history.anwsers[index],
+      correct:
+        history.quiz.questions[index].options[
+          getCorrectAnswer(history.quiz.questions[index].correctAnswer) - 1
+        ],
+      selected:
+        history.quiz.questions[index].options[
+          getCorrectAnswer(history.anwsers[index]) - 1
+        ],
+    };
   });
 
   res.status(200).json({
@@ -110,12 +118,14 @@ exports.fetchDetailHistory = asyncHandler(async (req, res, next) => {
 });
 
 function getCorrectAnswer(character) {
-    const charCode = character.charCodeAt(0);
-    if (charCode >= 65 && charCode <= 90) { // Uppercase letters (A-Z)
-      return charCode - 64;
-    } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters (a-z)
-      return charCode - 96;
-    } else {
-      return 0
-    }
+  const charCode = character.charCodeAt(0);
+  if (charCode >= 65 && charCode <= 90) {
+    // Uppercase letters (A-Z)
+    return charCode - 64;
+  } else if (charCode >= 97 && charCode <= 122) {
+    // Lowercase letters (a-z)
+    return charCode - 96;
+  } else {
+    return 0;
+  }
 }
--- models/model.history.js
+++ models/model.history.js
@@ -1,33 +1,50 @@
 const mongoose = require('mongoose');
 
+const Answer = new mongoose.Schema({
+
+  quiz: {
+    type: mongoose.Schema.Types.ObjectId,
+     ref: 'Question'
+  },
+
+  selectedOption: {
+    type: String,
+    required: [true, 'Please add Answers list'],
+    enum: [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F'
+    ]
+  },
+}
+);
+
 const History = new mongoose.Schema({
 
-      anwsers: {
-        type: [String],
+      quiz: {
+        type: mongoose.Schema.Types.ObjectId,
+         ref: 'Quiz'
+      },
+     anwsers: {
+        type: [Answer],
         required: [true, 'Please add Answers list'],
-        enum: [
-          'A',
-          'B',
-          'C',
-          'D',
-          'E',
-          'F'
-        ]
       },
       user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
       },
-      quiz: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Quiz'
-        },
+     
       timestamp: {
         type: Date,
         default: Date.now
       }
     }
 );
+
+
 
 
   module.exports = mongoose.model('History', History);
