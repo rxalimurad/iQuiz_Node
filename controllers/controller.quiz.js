debba1dddf8a068162a8816f04d793a874c01e6c
Ali Murad <rxalimurad@gmail.com>
2023-12-17 02:29:58 +0500

    Update controller.quiz.js

--- controllers/controller.quiz.js
+++ controllers/controller.quiz.js
@@ -2,21 +2,50 @@ const Quiz = require('../models/model.quiz');
 const asyncHandler = require('../middlewares/middleware.async');
 const Question = require('../models/model.question');
 const ErrorResponse = require('../utils/errorResponse'); 
+const jwt = require("jsonwebtoken");
+const User = require('../models/model.user');
+
 exports.fetchAllQuiz = asyncHandler(async (req, res, next) => {
-    let data = await Quiz.find().populate('questions').populate('history');
-    if (!data) {
-        return next(new ErrorResponse(`no quiz found`), 404)
+
+    if (req.headers.authorization) {
+        let token = req.headers.authorization.split(" ")[1];
+        let decoded = jwt.verify(token, process.env.JWT_SECRET);
+        let user = await User.findOne({ phone: decoded.phone }).populate('histories');
+        let histories = user.histories;
+        console.log(histories);
+        let data = await Quiz.find().populate('questions').populate('history');
+        if (!data) {
+            return next(new ErrorResponse(`no quiz found`), 404)
+        }
+        //quiz.history.length === 0 ? null : quiz.history[quiz.history.length - 1].id,
+        const formattedData = data.map(quiz => ({
+            _id: quiz._id,
+            name: quiz.name,
+            attempted: histories.length !== 0 ? histories[histories.length - 1].id : null,
+            timestamp: quiz.timestamp,
+            totalQuestions: quiz.questions.length,
+            id: quiz.id
+        }));
+
+res.status(200).json({ success: true, count: data.length, data: formattedData })
+
+    } else {
+        let data = await Quiz.find().populate('questions').populate('history');
+        if (!data) {
+            return next(new ErrorResponse(`no quiz found`), 404)
+        }
+        const formattedData = data.map(quiz => ({
+            _id: quiz._id,
+            name: quiz.name,
+            timestamp: quiz.timestamp,
+            totalQuestions: quiz.questions.length,
+            id: quiz.id
+        }));
+        res.status(200).json({ success: true, count: data.length, data: formattedData })
     }
-    const formattedData = data.map(quiz => ({
-        _id: quiz._id,
-        name: quiz.name,
-        attempted: quiz.history.length === 0 ? null : quiz.history[quiz.history.length - 1].id,
-        timestamp: quiz.timestamp,
-        totalQuestions: quiz.questions.length,
-        id: quiz.id
-    }));
-    res.status(200).json({ success: true, count: data.length, data: formattedData })
 })
+
+
 
 exports.addQuiz = asyncHandler(async (req, res, next) => {
     const quiz = await Quiz.create(req.body);
